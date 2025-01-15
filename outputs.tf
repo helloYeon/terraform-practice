6f4b84c7ebcdb3ed4061f8904d8685790bf0b44c
YEON.JINUN / ヨン.ジヌン <jinun.yeon@yeonce.com>
2025-01-15 17:30:22 +0900

    a

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,9 @@
-output "public_ip" {
-  value = aws_instance.name.public_ip
-  description = "APPLE_BANANA_PINE"
+# output "public_ip" {
+#   value = aws_instance.name.public_ip
+#   description = "APPLE_BANANA_PINE"
+# }
+
+output "alb_dns_name" {
+  value = aws_lb.example.dns_name
+  description = "The domain name of the load balance"
 }
